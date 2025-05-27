bae53960ca4ff2b0d46ab073bff44523c3ad9ce5
Radoslav Bodó <radoslav.bodo@igalileo.cz>
2025-05-27 08:24:16 +0200

    bump version

--- perccli_status.py
+++ perccli_status.py
@@ -30,7 +30,7 @@ import sys
 from argparse import ArgumentParser
 
 
-__version__ = "0.1"
+__version__ = "0.2"
 logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
 logger = logging.getLogger(__name__)
 
